51341fd0c0c69f218aa49967c3aa84fe93648449
pbhowelljr <pbhowelljr@gmail.com>
2016-04-15 14:00:53 -0700

    final before present

--- javascript/products.js
+++ javascript/products.js
@@ -3,42 +3,42 @@ var cardContent = "";
 console.log(productsButton);
 var cardArray = [{
   name: "Plateosaurus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "1",
   url: "/images/plateosaurus.jpeg"
 },{
   name: "Stegosaurus",
-  desctiption: "",
+  desctiption: "Wolf before they sold out.",
   ferocity: "3",
   url: "/images/stegosaurus.jpg"
 },{
   name: "Styracosaurus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "5",
   url: "/images/Styracosaurus.jpg"
 },{
   name: "Tyrannosaurus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "5",
   url: "/images/Tyrannosaurus.jpeg"
 },{
   name: "Allosaurus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "1",
-  url: "/images/Allosaurus.jpeg"
+  url: "/images/Allosaurus.jpg"
 },{
   name: "Velociraptor",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "4",
   url: "/images/Velociraptor.jpg"
 },{
   name: "Albertosaurus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "1",
   url: "/images/Albertosaurus.jpeg"
 },{
   name: "Euoplocephalus",
-  desctiption: "Readymade wolf before they sold out.",
+  desctiption: "Wolf before they sold out.",
   ferocity: "2",
   url: "/images/Euoplocephalus.jpeg"
 }];
@@ -50,7 +50,6 @@ var cardMaker = function(card){
   var cardHtml = "<article class=\"cards\">";
   cardHtml += "<h1>" + card.name + "</h1>";
   cardHtml += "<p>" + card.desctiption + "</p>";
-  cardHtml += "<p>" + card.ferocity + "</p>";
   cardHtml += "<img src=\"" + card.url + "\" alt=\"DinoDudes\">";
   cardHtml += "</article>";
   return cardHtml
