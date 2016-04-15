e7d8ca3c53f181ad3123f0bfe0c9f015d644c3a7
pbhowelljr <pbhowelljr@gmail.com>
2016-04-15 00:29:17 -0700

    render content in mainContent div with javascript using a button w/ a click eventListener attached

---
+++ javascript/home.js
@@ -0,0 +1,46 @@
+var mainContentDiv = document.getElementById('mainContent');
+var homeButton = document.getElementById('homeButton');
+console.log(homeButton);
+var homeCreator = function() {
+	mainContentDiv.innerHTML = (
+		'<div id="homeLeftColumn" class="homeColumn">'+
+			'<div class="homeReview" id="homeReview_1">'+
+				'<p>My dinosaur is no more or less ferocious than expected.</p>'+
+				'<p>--Customers</p>'+
+			'</div>'+
+			'<div class="homeReview" id="homeReview_2">'+
+				'<p>The racing dinos are unmatched in speed and agility!</p>'+
+				'<p>--Dino-racer</p>'+
+			'</div>'+
+		'</div>'+
+		'<div id="homeMiddleColumn" class="homeColumn">'+
+			'<img id="homeImage" src="images/homeDino.jpg" alt="Amazing Dinosaur">'+
+		'</div>'+
+		'<div id="homeRightColumn" class="homeColumn">'+
+			'<div class="homeReview" id="homeReview_3">'+
+				'<p>Easy to read ferocity levels makes dinosaur buying easy!</p>'+
+				'<p>--Experts</p>'+
+			'</div>'+
+			'<div class="homeReview" id="homeReview_4">'+
+				'<p>An investment in dinosaurs is an investment in the future.</p>'+
+				'<p>--Dinosaurs</p>'+
+			'</div>'+
+		'</div>'
+	);
+} 
+
+homeButton.addEventListener("click", homeCreator, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
